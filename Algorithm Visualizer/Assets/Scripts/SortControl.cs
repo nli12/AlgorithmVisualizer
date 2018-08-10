07057d9805e5ae19b81ebab75d9decaa18bf85c5
nli12 <nathanli117@gmail.com>
2018-08-10 15:52:58 -0700

    Additonal comments and minor refactoring

--- Algorithm Visualizer/Assets/Scripts/SortControl.cs
+++ Algorithm Visualizer/Assets/Scripts/SortControl.cs
@@ -12,10 +12,10 @@ public class SortControl : MonoBehaviour
     readonly int wrongNoisePos = 3;
     readonly int partitionNoisePos = 4;
     readonly int completeNoisePos = 5;
-    readonly int checkNoisePos = 6;
 
     int count;
     float swapTime = 0.7f;
+    float defaultY = 0.25f;
 
     public Transform templateCube;
     public string type;
@@ -31,14 +31,12 @@ public class SortControl : MonoBehaviour
     int selectedOne = -1;
     int selectedTwo = -1;
 
-    float defaultY = 0.25f;
-
-
+    
 
     void Start()
     {
-
-        count = 9;
+        //Set default number of cubes
+        count = 7;
 
         sort = DOTween.Sequence();
         System.Random rnd = new System.Random();
@@ -108,7 +106,7 @@ public class SortControl : MonoBehaviour
 
     Sequence CompletedAnimation()
     {
-        //Animation to be played when sort is completed
+        // Animation to be played when sort is completed
         Sequence animation = DOTween.Sequence();
         foreach (Transform cube in cubes)
         {
@@ -126,41 +124,46 @@ public class SortControl : MonoBehaviour
     public void BubbleSort()
 
     {
+        // Set all cubes to yellow
         foreach (Transform cube in cubes)
         {
             sort.Join(cube.GetComponent<Renderer>().material.DOColor(Color.yellow, 0.01f));
         }
 
+        // Execute bubble sort
         for (int i = 0; i < values.Length; i++)
         {
             for (int j = 0; j < values.Length - i - 1; j++)
             {
-
+                // Set current cube being examined to red
                 sort.Append(cubes[j].GetComponent<Renderer>().material.DOColor(Color.red, 0.1f));
-
-                //sort.AppendCallback(() => { cubes[j].GetChild(1).GetComponent<AudioSource>().Play();});
-
                 sort.AppendInterval(0.3f);
 
+                // Swap with neighbor if greater than neighbor
                 if (values[j] > values[j + 1])
                 {
                     sort.Append(Swap(j, j + 1));
-                    //Debug.Log(j + " " + (j + 1));
                 }
+                // Reset current cube to yellow if less than neighbor
                 else
                 {
                     sort.Append(cubes[j].GetComponent<Renderer>().material.DOColor(Color.yellow, 0.1f));
                 }
             }
+
+            // Set cube at end of iteration to green 
             sort.Append(cubes[values.Length - i - 1].GetComponent<Renderer>().material.DOColor(Color.green, 0.1f));
         }
 
     }
 
+    // Create a queue of sets representing the swaps necessary to complete the bubble sort,
+    // where each set contains the indices of the two elements that should be swapped at that step
     private void BubbleSequence()
     {
-        seq = new Queue<HashSet<int>>();
+        
         int[] valuesDup = (int[])values.Clone();
+        seq = new Queue<HashSet<int>>();
         HashSet<int> temp;
         int tempValue;
 
@@ -173,7 +176,6 @@ public class SortControl : MonoBehaviour
                     temp = new HashSet<int>();
                     temp.Add(j);
                     temp.Add(j + 1);
-                    //Debug.Log(j + " " + (j+1));
                     seq.Enqueue(temp);
 
                     tempValue = valuesDup[j];
@@ -194,6 +196,8 @@ public class SortControl : MonoBehaviour
         //if (component) component.text = index.ToString();
         //if (component) component.text = component.text + index.ToString();
 
+
+        // Do nothing if the sort has been completed 
         if (seq.Count == 0)
         {
             return; 
@@ -201,26 +205,31 @@ public class SortControl : MonoBehaviour
 
 
         Sequence animation = DOTween.Sequence();
-        animation.Append(cubes[index].GetComponent<Renderer>().material.DOColor(Color.yellow, 0.1f));
-        
 
+        // Set currently selected cube to yellow 
+        animation.Append(cubes[index].GetComponent<Renderer>().material.DOColor(Color.yellow, 0.1f));
+
+        // If the same cube has been selected twice, reset both selections
         if (selectedOne == index)
         {
             selectedOne = -1;
             selectedTwo = -1;
+            animation.Append(cubes[index].GetComponent<Renderer>().material.DOColor(Color.white, 0.1f));
         }
-
-        if (selectedOne == -1 && selectedTwo == -1)
+        // If no cubes have been selected, store the index of the first cube selected
+        else if (selectedOne == -1 && selectedTwo == -1)
         {
             selectedOne = index;
 
-        } else if (selectedOne != -1 && selectedOne != index && selectedTwo == -1)
+        }
+        // If the second cube is being selected, check if the pair indices are next in the swap sequence 
+        else if (selectedOne != -1 && selectedOne != index && selectedTwo == -1)
         {
             selectedTwo = index;
             if (seq.Peek().Contains(selectedOne) && seq.Peek().Contains(selectedTwo))
             {
                 //Correct pair animation
-                animation.AppendCallback(() => { cubes[selectedOne].GetChild(completeNoisePos).GetComponent<AudioSource>().Play(); });
+                animation.AppendCallback(() => { cubes[selectedOne].GetChild(correctNoisePos).GetComponent<AudioSource>().Play(); });
                 animation.Join(cubes[selectedOne].GetComponent<Renderer>().material.DOColor(Color.green, 0.1f));
                 animation.Join(cubes[selectedTwo].GetComponent<Renderer>().material.DOColor(Color.green, 0.1f));
                 animation.AppendInterval(0.5f);
@@ -237,13 +246,14 @@ public class SortControl : MonoBehaviour
             } else
             {
                 //Wrong pair animation
-                animation.AppendCallback(() => { cubes[selectedOne].GetChild(completeNoisePos).GetComponent<AudioSource>().Play(); });
+                animation.AppendCallback(() => { cubes[selectedOne].GetChild(wrongNoisePos).GetComponent<AudioSource>().Play(); });
                 animation.Join(cubes[selectedOne].GetComponent<Renderer>().material.DOColor(Color.red, 0.1f));
                 animation.Join(cubes[selectedTwo].GetComponent<Renderer>().material.DOColor(Color.red, 0.1f));
                 animation.AppendInterval(0.8f);
                 animation.Append(cubes[selectedOne].GetComponent<Renderer>().material.DOColor(Color.white, 0.1f));
                 animation.Join(cubes[selectedTwo].GetComponent<Renderer>().material.DOColor(Color.white, 0.1f));
             }
+            // Reset both selections 
             selectedOne = -1;
             selectedTwo = -1;
         }
@@ -266,29 +276,28 @@ public class SortControl : MonoBehaviour
         int pivot = values[j];
         int small = i - 1;
 
-       
-
-        //Highlight the section of the array we're currently focusing on in yellow
+        // Highlight the current partition in yellow
         sort.Append(cubes[i].GetComponent<Renderer>().material.DOColor(Color.yellow, 0.01f));
         for (int a = i+1; a <= j; a++)
         {
             sort.Join(cubes[a].GetComponent<Renderer>().material.DOColor(Color.yellow, 0.01f));
         }
 
-        //Highlight the pivot
+        // Highlight the pivot
         sort.AppendCallback(() => { cubes[j].GetChild(partitionNoisePos).GetComponent<AudioSource>().Play(); });
         sort.Append(cubes[j].GetComponent<Renderer>().material.DOColor(Color.magenta, 0.1f));
 
+        // Iterate through the current partition 
         for (int k = i; k < j; k++)
         {
             sort.Append(cubes[k].GetComponent<Renderer>().material.DOColor(Color.red, 0.1f));
             sort.AppendInterval(0.4f);
+            // Highlight in blue if less than the pivot 
             if (values[k] <= pivot)
             {
                 small++;
                 if (k != small)
                 {
-                    
                     sort.Append(cubes[k].GetComponent<Renderer>().material.DOColor(Color.blue, 0.1f));
                     sort.Append(Swap(k, small));
                 }
@@ -299,12 +308,15 @@ public class SortControl : MonoBehaviour
 
                 sort.Append(cubes[small].GetComponent<Renderer>().material.DOColor(Color.blue, 0.1f));
             }
+            // Set back to yellow if greater than the pivot 
             else
             {
                 sort.Append(cubes[k].GetComponent<Renderer>().material.DOColor(Color.yellow, 0.1f));
             }
 
         }
+
+        // Swap the pivot with the first element in the partition greater than the pivot 
         if (j != small + 1)
         {
             sort.Append(Swap(j, small + 1));
@@ -329,9 +341,9 @@ public class SortControl : MonoBehaviour
 
     Sequence Swap(int a, int b)
     {
-
         int left, right;
 
+        // Ensures the left cube always goes up and the right always goes down 
         if (a < b)
         {
             left = a;
@@ -343,6 +355,7 @@ public class SortControl : MonoBehaviour
             right = a;
         }
 
+        // Generates swap animations 
         Sequence cubeUp = DOTween.Sequence();
         cubeUp.AppendCallback(() => { cubes[left].GetChild(swapNoisePos).GetComponent<AudioSource>().Play(); });
         cubeUp.Join(cubes[left].DOMoveY(defaultY + 0.33f, swapTime));
@@ -364,6 +377,7 @@ public class SortControl : MonoBehaviour
 
     }
 
+    // Swaps the values in the underlying data structures 
     private void DataSwap(int a, int b)
     {
         Transform tempCube = cubes[a];
@@ -380,8 +394,8 @@ public class SortControl : MonoBehaviour
 
     public void HoverUp(Transform cube)
     {
-            Sequence seq = DOTween.Sequence();
-            seq.Append(cube.DOMoveY(defaultY + 0.10f, 0.3f));
+        Sequence seq = DOTween.Sequence();
+        seq.Append(cube.DOMoveY(defaultY + 0.10f, 0.3f));
     }
 
     public void HoverDown(Transform cube)
